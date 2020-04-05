32de89bc972d763bef0eb8e63f2b08f85ae91f39
loboser <loboser.sl@gmail.com>
2020-04-05 08:42:46 -0300

    Cambios en tp2_1_1.cpp

--- tp2_1_1.cpp
+++ tp2_1_1.cpp
@@ -5,15 +5,18 @@
 #define N 4
 #define M 5
 
+int f,c;
+double mt[N][M];
+
 int main(){
-    int f,c;
-    double mt[N][M];
+    
     for(f = 0;f<N; f++){
         for(f = 0;f<N; f++){
-            cprintf("%lf ", mt[f][c]);
+            printf("%lf ", mt[f][c]);
         }
-        cprintf("\n");
+        printf("\n");
     }
+    getchar(); //Pausa el programa
     return 0;
 }
 
